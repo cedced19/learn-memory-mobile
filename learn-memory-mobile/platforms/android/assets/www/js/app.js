95789d0ef8568fa4862d2f1d70a1c8a5c7dda716
Cédric <cedced19@gmail.com>
2015-07-27 22:14:07 +0200

    Initial commit

---
+++ learn-memory-mobile/platforms/android/assets/www/js/app.js
@@ -0,0 +1,14 @@
+﻿angular.module('LearnMemory', ['ngRoute'])
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'LearnMemoryHomeCtrl'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+}])
+.controller('LearnMemoryHomeCtrl', function ($scope, $location, $http) {
+
+});
---
+++ learn-memory-mobile/platforms/android/assets/www/js/init-ui.js
@@ -0,0 +1,18 @@
+(function ($) {
+    var body = $('body');
+
+    $('#header__icon').click(function (e) {
+        e.preventDefault();
+        body.toggleClass('with--sidebar');
+    });
+
+    $('#site-cache').click(function () {
+        body.removeClass('with--sidebar');
+    });
+
+    $('.menu > a').click(function () {
+        body.removeClass('with--sidebar');
+    });
+
+
+})(jQuery);
