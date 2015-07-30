acaa5605297b3153f7931c9d87b24b2e70bc4fdb
Cédric <cedced19@gmail.com>
2015-07-30 11:14:12 +0200

    Get it offline

--- learn-memory-mobile/www/js/app.js
+++ learn-memory-mobile/www/js/app.js
@@ -13,6 +13,14 @@
         templateUrl: 'views/lesson.html',
         controller: 'LearnMemoryDownloadItemCtrl'
     })
+    .when('/offline', {
+        templateUrl: 'views/list.html',
+        controller: 'LearnMemoryOfflineCtrl'
+    })
+    .when('/offline/:id', {
+        templateUrl: 'views/lesson.html',
+        controller: 'LearnMemoryOfflineItemCtrl'
+    })
     .when('/config', {
         templateUrl: 'views/config.html',
         controller: 'LearnMemoryConfigCtrl'
@@ -23,16 +31,18 @@
 }])
 .controller('LearnMemoryHomeCtrl', function ($scope, $rootScope, $location, $localStorage) {
     $localStorage.$default({
-        url: '',
+        adress: '',
         offline: []
     });
 
+    $rootScope.download = false;
+
     $rootScope.showMenu = function () {
-        document.getElementsByTagName('body')[0].classList.add('with--sidebar');
+        document.getElementsByTagName('body')[0].classList.add('with-sidebar');
     };
 
     $rootScope.hideMenu = function (path) {
-        document.getElementsByTagName('body')[0].classList.remove('with--sidebar');
+        document.getElementsByTagName('body')[0].classList.remove('with-sidebar');
         if (path) {
             $location.path('/' + path);
         }
@@ -59,19 +69,47 @@
             $location.path('/download/' + item.id);
         };
 
+        $rootScope.download = function () {
+            $http.get('http://' + $localStorage.adress + '/api/long').success(function (data) {
+                $localStorage.offline = data;
+                $location.path('/offline');
+            });
+        };
+
     }).error(function () {
         $location.path('/offline');
     });
 }).controller('LearnMemoryDownloadItemCtrl', function ($scope, $rootScope, $location, $localStorage, $http, $routeParams) {
     $http.get('http://' + $localStorage.adress + '/api/' + $routeParams.id).success(function (data) {
         $rootScope.nav = false;
+        $rootScope.download = false;
 
         $scope.item = data;
+
     }).error(function () {
-        $location.path('/offline');
+        $location.path('/offline/' + $routeParams.id);
+    });
+}).controller('LearnMemoryOfflineCtrl', function ($scope, $rootScope, $location, $localStorage, $http) {
+    $rootScope.nav = 'offline';
+    $rootScope.download = false;
+
+    $scope.items = $localStorage.offline;
+
+    $scope.goItem = function (item) {
+        $location.path('/offline/' + item.id);
+    };
+}).controller('LearnMemoryOfflineItemCtrl', function ($scope, $rootScope, $location, $localStorage, $http, $routeParams) {
+    $rootScope.nav = false;
+    $rootScope.download = false;
+
+    angular.forEach($localStorage.offline, function (value, key) {
+        if (value.id == $routeParams.id) {
+            $scope.item = value;
+        }
     });
 }).controller('LearnMemoryConfigCtrl', function ($scope, $rootScope, $location, $localStorage) {
     $rootScope.nav = 'config';
+    $rootScope.download = false;
 
     $scope.adress = $localStorage.adress;
 
